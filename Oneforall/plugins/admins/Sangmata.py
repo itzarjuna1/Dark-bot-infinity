3a6c31edef84b9e5445d14ae13f2873daaa7cba3
Gxinfinity <190551265+Gxinfinity@users.noreply.github.com>
2025-05-24 15:44:32 +0530

    Update Sangmata.py

--- Oneforall/plugins/admins/Sangmata.py
+++ Oneforall/plugins/admins/Sangmata.py
@@ -12,43 +12,59 @@ from Oneforall.core.userbot import assistants
 
 @app.on_message(filters.command("sg"))
 async def sg(client: Client, message: Message):
-    if len(message.text.split()) < 1 and not message.reply_to_message:
-        return await message.reply("sg username/id/reply")
+    # Send initial processing message
+    processing_msg = await message.reply("<code>Processing...</code>")
+
+    # Check if the user is passed as reply or as argument
     if message.reply_to_message:
-        args = message.reply_to_message.from_user.id
+        user_id = message.reply_to_message.from_user.id
+    elif len(message.command) > 1:
+        user_id = message.command[1]
     else:
-        args = message.text.split()[1]
-    lol = await message.reply("<code>Processing...</code>")
-    if args:
-        try:
-            user = await client.get_users(f"{args}")
-        except Exception:
-            return await lol.edit("<code>Please specify a valid user!</code>")
-    bo = ["sangmata_bot", "sangmata_beta_bot"]
-    sg = random.choice(bo)
+        return await processing_msg.edit("Usage: <code>/sg [username/user_id] or reply to a user</code>")
+
+    # Try to get the user object
+    try:
+        user = await client.get_users(user_id)
+    except Exception:
+        return await processing_msg.edit("<code>Invalid user specified.</code>")
+
+    # Choose one of the sangmata bots randomly
+    sangmata_bots = ["sangmata_bot", "sangmata_beta_bot"]
+    selected_bot = random.choice(sangmata_bots)
+
+    # Select the userbot assistant
     if 1 in assistants:
         ubot = us.one
+    else:
+        return await processing_msg.edit("<code>Userbot assistant not found.</code>")
 
+    # Send the user ID to the bot
     try:
-        a = await ubot.send_message(sg, f"{user.id}")
-        await a.delete()
+        sent_msg = await ubot.send_message(selected_bot, str(user.id))
+        await sent_msg.delete()
     except Exception as e:
-        return await lol.edit(e)
+        return await processing_msg.edit(f"<code>{str(e)}</code>")
+
     await asyncio.sleep(1)
 
-    async for stalk in ubot.search_messages(a.chat.id):
-        if stalk.text == None:
-            continue
-        if not stalk:
-            await message.reply("botnya ngambek")
-        elif stalk:
-            await message.reply(f"{stalk.text}")
+    # Search for the bot's reply
+    found = False
+    async for msg in ubot.search_messages(selected_bot):
+        if msg and msg.text:
+            await message.reply(msg.text)
+            found = True
             break
 
+    if not found:
+        await message.reply("The bot did not return any data.")
+
+    # Try to clear the message history with the bot
     try:
-        user_info = await ubot.resolve_peer(sg)
-        await ubot.send(DeleteHistory(peer=user_info, max_id=0, revoke=True))
+        peer = await ubot.resolve_peer(selected_bot)
+        await ubot.send(DeleteHistory(peer=peer, max_id=0, revoke=True))
     except Exception:
         pass
 
-    await lol.delete()
+    # Remove the processing message
+    await processing_msg.delete()
