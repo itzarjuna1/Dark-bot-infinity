18ee22b532900199509085e44829ac4e61668fc4
Gxinfinity <190551265+Gxinfinity@users.noreply.github.com>
2025-06-24 14:48:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Gxinfinity/musicgoku",
+    "https://github.com/Gxinfinity/Dark-bot-infinity",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
