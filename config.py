eb33e2227b6946ae4c710078308aeb460369f686
Gxinfinity <190551265+Gxinfinity@users.noreply.github.com>
2025-07-03 14:40:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ API_ID = int(getenv("API_ID", "20346025"))
 API_HASH = getenv("API_HASH", "d34558796860a173cb5c918968fe85a0")
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN","7684739810:AAGe-8XTSw-7rCcDqis5h8t7Ppmb4ICLHsM")
+BOT_TOKEN = getenv("BOT_TOKEN","7684739810:AAGewbTxtQMuCY59DlrQyHt4ANjNrgz07eQ")
 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://knight4563:knight4563@cluster0.a5br0se.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
