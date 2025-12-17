1c51478bc15269ce6d51a7b8f04061d8d1c50664
ARJUN <pachangedinanath@gmail.com>
2025-12-17 15:39:13 +0530

    Update __init__.py
    Signed-off-by: ARJUN  <pachangedinanath@gmail.com>

--- Oneforall/utils/__init__.py
+++ Oneforall/utils/__init__.py
@@ -1,6 +1,6 @@
 from .channelplay import *
 from .database import *
-#from .decorators import *
+from .decorators import *
 from .extraction import *
 from .formatters import *
 from .inline import *
