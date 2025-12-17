97c8b132446699a52448390a3bdc78fb172dadd2
ARJUN <pachangedinanath@gmail.com>
2025-12-17 16:44:09 +0530

    Update __init__.py
    Signed-off-by: ARJUN  <pachangedinanath@gmail.com>

--- Oneforall/utils/decorators/__init__.py
+++ Oneforall/utils/decorators/__init__.py
@@ -1,5 +1,5 @@
-from .admin import *
-from .language import *
-from .play import *
-from .userbotjoin import *
-from .sudo import *
+from .admin import*
+from .language import*
+from .play import*
+from .userbotjoin import*
+from .sudo import*
